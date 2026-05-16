a3d34d894bef417eb32c350b253a6ed48a6c756c
Alexander Asanov <aleksandro.eterverda@gmail.com>
2026-05-17 00:32:36 +0300

    Removed host-gio

--- host-gio/driver/rssi/rssi.go
+++
@@ -1,7 +0,0 @@
-// Package rssi provides RSSI (Received Signal Strength Indicator) sensor interface
-package rssi
-
-// Hello returns greeting from RSSI sensor
-func Hello() string {
-	return "Hello Rssi"
-}

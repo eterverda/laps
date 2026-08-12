8cadeed1258f1efa3ad2af7b093862f0616962c2
Alexander Asanov <aleksandro.eterverda@gmail.com>
2026-08-13 01:27:24 +0300

    Switched to env_logger

--- host/src/main.rs
+++ host/src/main.rs
@@ -17,21 +17,12 @@ enum Commands {
 }
 
 fn setup_logging() {
-    use simplelog::*;
-    use time::macros::format_description;
-    TermLogger::init(
-        LevelFilter::Debug,
-        ConfigBuilder::new()
-            .set_time_format_custom(format_description!(
-                "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3][offset_hour sign:mandatory]:[offset_minute]"
-            ))
-            .set_time_offset_to_local()
-            .unwrap()
-            .build(),
-        TerminalMode::Mixed,
-        ColorChoice::Auto,
-    )
-    .unwrap();
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Debug)
+        .filter_module("naga", log::LevelFilter::Warn)
+        .filter_module("wgpu", log::LevelFilter::Warn)
+        .format_timestamp_millis()
+        .init();
 }
 
 fn main() {

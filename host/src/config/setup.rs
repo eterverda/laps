121c56abc925a31081973749b465d2b089fa9727
Alexander Asanov <aleksandro.eterverda@gmail.com>
2026-09-25 01:34:06 +0300

    mod.rs + tests.rs -> mod

--- host/src/config/setup.rs
+++ host/src/config/setup.rs
@@ -43,4 +43,20 @@ impl ColorConfig {
 }
 
 #[cfg(test)]
-mod tests;
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_viewport_defaults_to_full_frame() {
+        let fpv: FpvConfig = serde_yml::from_str("camera-id: camera-1\n").unwrap();
+        assert_eq!(
+            fpv.viewport,
+            super::super::camera::ViewportConfig {
+                left: 0.0,
+                top: 0.0,
+                right: 1.0,
+                bottom: 1.0,
+            }
+        );
+    }
+}
